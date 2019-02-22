70f4e45870d221ad84181976572b5378978ba8bb
credd3 <creddy4@sapient.com>
2019-02-22 18:45:16 +0530

    Handlebars using gulp --- Compiling Partials and Helpers through gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,26 @@ var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
 var data = require('gulp-data');
 
-gulp.task('hbs', () => { 
+gulp.task('hbs', () => {
+    let options = {
+        batch : ['./src/hbs/partials'],
+        helpers : {
+            customEach : (context,options) => {
+                let data = ``;
+
+                context.forEach(element => {
+                    data += options.fn(element);
+                });
+
+                return data;
+            }
+        }
+    }
     return gulp.src(`src/hbs/*.hbs`)
     .pipe(data((file) => {
         return require('./src/data/cast.json');
     }))
-    .pipe(handlebars())
+    .pipe(handlebars({},options))
     .pipe(rename('index.html'))
     .pipe(gulp.dest(`src/html`))
 })
