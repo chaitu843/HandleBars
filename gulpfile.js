ff56cd65d5ffbb846f9dc3e0ee87b85b57771f0e
credd3 <creddy4@sapient.com>
2019-02-22 17:53:56 +0530

    Handlebars Using Gulp -- Passing Data

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var handlebars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
+var data = require('gulp-data');
+
+gulp.task('hbs', () => { 
+    return gulp.src(`src/hbs/*.hbs`)
+    .pipe(data((file) => {
+        return require('./src/data/cast.json');
+    }))
+    .pipe(handlebars())
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest(`src/html`))
+})
 
 gulp.task('connect', function(){
     connect.server();
 });
 
-gulp.task('default', ['connect']);
+gulp.task('default', ['hbs','connect']);
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,3 @@
 $(document).ready(() => {
 
-    Handlebars.registerPartial('characterPartial', `<li>
-    <h2>{{name}}</h2>
-    <p>Actor : {{actor}}</p>
-    <p>House : {{house}}</p>
-    {{#if location}}<p>WhereAbouts : {{location}}</p>{{/if}}
-</li>`);
-    let compiledCharacterTemplate = Handlebars.templates['character-list.hbs'];
-    console.log(compiledCharacterTemplate);
-
-    $.ajax("../data/cast.json").done(cast => {
-        $('.character-list').html(compiledCharacterTemplate(cast));
-    });
 })
